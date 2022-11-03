0b44c46cac235cd343ef8555f9c92317ec69040c
your1024 <gettomorrow@qq.com>
2022-11-03 09:58:57 +0800

    更新

--- src/Facades/MyStr.php
+++ src/Facades/MyStr.php
@@ -2,7 +2,7 @@
 namespace Yhy\Str\Facades;
 use Illuminate\Support\Facades\Facade;
 
-class MyStrFacade extends Facade
+class MyStr extends Facade
 {
     protected static function getFacadeAccessor(): string
     {
